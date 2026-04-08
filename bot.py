0e0638ae7afa93d76b1aae8a3b535e00e9bc0db1
𝐀𝐘𝐔𝐒𝐇 𝐊𝐔𝐌𝐀𝐑 𝐘𝐀𝐃𝐀𝐕 <pragati.prajapati2009@gmail.com>
2026-04-08 12:39:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ from pyrogram.errors import (
 # ---------------------------------------------------------------------
 
 BOT_TOKEN = os.getenv('BOT_TOKEN', '8588199256:AAGUjtP_MvXCUGctOoBfMX1-eG2nV3ATCwY')
-ADMIN_IDS = [int(x.strip()) for x in os.getenv('ADMIN_ID', '6042317029,8441236350').split(',')]
+ADMIN_IDS = [int(x.strip()) for x in os.getenv('ADMIN_ID', '8441236350').split(',')]
 
 MONGO_URL = os.getenv('MONGO_URL', 'mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority')
 API_ID = int(os.getenv('API_ID', '36326629'))
